0eb15a80e30b0445a92c01d70d699c526103ad80
Kartoshka <140354632+kartoshka08@users.noreply.github.com>
2023-10-30 15:24:30 +0300

    Update Randoms.java

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,36 +1,53 @@
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random = new Random();
+    protected Random random= new Random();
+    List<Integer> randomList = new ArrayList<>();
 
     public Randoms(int min, int max) {
-        random.nextInt(min, max);
+        for (int i = 0; i < 1000; i ++){
+            randomList.add(random.nextInt(min, max+1));
+        }
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
-            boolean isExist = true;
-            int next;
+            boolean isList = true;
+            int next = 0;
+
+
             @Override
             public boolean hasNext() {
-                if (isExist){
-                    if (next == 0){
-                        return false;
-                    }else return true;
-                }
-                else return false;
+                if (isList) {
+                    if (next < randomList.size()) {
+                        return true;
+                    } else return isList;
+                } else return false;
             }
 
             @Override
             public Integer next() {
-                if (isExist) {
-                    if (next == 0) {
-                        next = random.nextInt();
+                if (isList) {
+                    if (next < randomList.size()){
+                        int nextRan = randomList.get(next);
+                        next++;
+                        return nextRan;
+                    }else{
+                        int nextRan = randomList.get(0);
+                        next = 1;
+                        return nextRan;
                     }
                 }
-                return next;
+                else {
+                    isList = false;
+                    int nextRan = 0;
+                    next = 0;
+                    return nextRan;
+                }
             }
         };
     }
